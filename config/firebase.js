9ac123ba893077b66cf6b7dd333c6bb6fb3605df
MatthewEnamuotor <90224141+oviematthew@users.noreply.github.com>
2025-05-27 00:27:12 -0400

    made changes to structure

--- config/firebase.js
+++ config/firebase.js
@@ -2,15 +2,14 @@ import { initializeApp } from "firebase/app";
 import { initializeAuth, getReactNativePersistence } from "firebase/auth";
 import { getFirestore, getDocs, collection } from "firebase/firestore";
 import ReactNativeAsyncStorage from "@react-native-async-storage/async-storage";
-import { ApiKey } from "@env";
 
 const firebaseConfig = {
-  apiKey: ApiKey,
-  authDomain: "chatapp-a13b2.firebaseapp.com",
-  projectId: "chatapp-a13b2",
-  storageBucket: "chatapp-a13b2.appspot.com",
-  messagingSenderId: "791876059295",
-  appId: "1:791876059295:web:1e8e177095a4cb91b262aa",
+  apiKey: process.env.ApiKey,
+  authDomain: process.env.authDomain,
+  projectId: process.env.projectId,
+  storageBucket: process.env.storageBucket,
+  messagingSenderId: process.env.messagingSenderId,
+  appId: process.env.appId,
 };
 
 const app = initializeApp(firebaseConfig);
